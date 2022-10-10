b7627c9453ff1ca005b47df9fd21a8ee4846c885
ayomide <adebayoayomide396@gmail.com>
2022-10-10 16:24:11 +0100

    fixing errors

--- script.js
+++ script.js
@@ -101,8 +101,8 @@ var swiper = new Swiper(".fourth_section_img", {
      
     },
     1024: {
-      // spaceBetween: 5,
-      slidesPerView: 3,
+      spaceBetween: 5,
+      slidesPerView: 4,
       
      
     },
