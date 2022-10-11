110ebf7109582aa97271ef7991662c050f9d0c32
ayomide <adebayoayomide396@gmail.com>
2022-10-11 21:38:02 +0100

    autoPlay swiper

--- script.js
+++ script.js
@@ -94,10 +94,10 @@ var swiper = new Swiper(".fourth_section_img", {
   spaceBetween: 13,
   // spaceBetween: 30,
   centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
